3796e9599a3b613ba860f8328de2273707355c68
Minjea43 <112991705+Minjea43@users.noreply.github.com>
2022-09-26 10:38:52 +0900

    Add files via upload

--- chapter02/c_printf.c
+++ chapter02/c_printf.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include     <stdio.h>
 
 // 제어문자
-//      \n : 한줄 개행
-//      \t : 탭(8칸 스페이스)
-int main(void)
+//  + \n : 한줄 개행
+//  + \t : 탭(8칸 스페이스)
+int  main(void)
 {
     /* code */
-    printf("HELLO\n");
-    printf("Kim min jea");
-    
-    return 0;
+    printf("Hello\n");
+    printf("Yujun Choi");
+
+    return  0;
 }
