0a26e486cb244e43753bb52b3e8c5d32d56f897f
Minjea43 <112991705+Minjea43@users.noreply.github.com>
2022-11-16 10:34:35 +0900

    Add files via upload

---
+++ chapter07/c_method_recursive_pl4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+    int n = recursion(16);
+    printf("%d", n);
+}
+
+int recursion(int n){
+    // if 문 {실행문} -> 조건이 참인 경우 실행
+    // 실행문이 1개면 {}을 생략
+    if(n<5) return 1;
+    else if (n%5 == 1) return n+recursion(n-1);
+    else recursion(n-1);
+}
